394890a9383ad2ce7660fd4fef787bedd48f0c17
dheeraj <dheerajveluru10@gmail.com>
2026-01-28 15:55:04 +0530

    updated app with blueprints

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response, abort, g
+from flask import Flask, render_template, request, redirect, url_for, make_response, abort, g,Blueprint
 import jwt, datetime, sqlite3, bcrypt
 from functools import wraps
 from database import init_db,close_db
+from admin import admin_bp
+from member import member_bp
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "super-secret-key"
 
 
-
+app.register_blueprint(admin_bp)
+app.register_blueprint(member_bp)
 
 
 @app.teardown_appcontext
